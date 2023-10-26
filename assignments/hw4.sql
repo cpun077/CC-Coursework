001668fd18798b191185c6531c41da3fcb511246
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2023-10-26 12:03:26 -0700

    hw4 insert change

--- assignments/hw4.sql
+++ assignments/hw4.sql
@@ -7,7 +7,8 @@ where Name = 'San Jose' and CountryCode = 'USA' and District = 'California';
 
 select * from city where Name = 'San Jose' and CountryCode = 'USA';
 
-insert into city values ('4080', 'Twin Peaks', 'USA', 'Washington', '10500');
+insert into city (ID, Name, CountryCode, District, Population) 
+select max(ID)+1, 'Twin Peaks', 'USA', 'Washington State', '10500' from city;
 select * from city where Name = 'Twin Peaks';
 
 delete from city where Name = 'Twin Peaks';
