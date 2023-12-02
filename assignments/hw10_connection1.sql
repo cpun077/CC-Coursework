96a008d2e9a177105ea11aa1818e678b3cc1733f
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2023-12-01 20:13:30 -0800

    refactor hw10 + doesnt work

--- assignments/hw10_connection1.sql
+++ assignments/hw10_connection1.sql
@@ -3,5 +3,6 @@ set SQL_SAFE_UPDATES = 0;
 alter table city modify column countrycode varchar(4);
 
 select connection_id();
-select * from city lock in share mode;
+lock tables city read;
 insert into city values ('4080', 'Test', 'Test', 'Test', '111111');
+unlock tables;
---
+++ assignments/hw10_connection2.sql
@@ -0,0 +1,5 @@
+use cis44a;
+set SQL_SAFE_UPDATES = 0;
+
+select connection_id();
+insert into city values (4080,'Test','XXX','Test',111111);
