152d3fd8bcd646566e376fbba90e07f8695bd97a
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-01-09 13:02:31 -0800

    hello assignment

---
+++ DA-CIS22A/hello/hello_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World!" << endl;
+    cout << "My name is Caden"
+        << " and I like learning new things."
+        << endl;
+    return 0;
+}
