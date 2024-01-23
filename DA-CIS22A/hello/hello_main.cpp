048aeb65cfd203c7e4e67549c3a07424e2e09392
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-01-22 16:33:41 -0800

    writing some useful cpp stuff for later from zybooks hw

--- DA-CIS22A/hello/hello_main.cpp
+++ DA-CIS22A/hello/hello_main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
+// to run, cd C:\users\ then cd the username then the folder's dir
 int main () {
     cout << "Hello World!" << endl;
     cout << "My name is Caden"
         << " and I like learning new things."
         << endl;
+    double num = 1.21234;
+    // include iomanip, round num to x digits
+    cout << "Rounded to 3 decimals: " << fixed << setprecision(3) << num << endl;
+
+    const int root = 4.0;
+    cout << sqrt(root) << endl;
+    cout << fabs(-1) << endl;
+    cout << pow(2, 3) << endl;
+    cout << static_cast<int>(root) << endl;
+
     return 0;
 }
