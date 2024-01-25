79e55ba2468d2602f449bb234e7e117b31d981be
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-01-25 13:37:32 -0800

    add input and scanning data practice

--- fhda/cis44h/numcheck.R
+++ fhda/cis44h/numcheck.R
@@ -11,28 +11,63 @@ for (num in v) {
 
 # Extra stuff
 print("******************************")
-range_vector <- 1:10
 
+# %/% is integer division
+
+range_vector <- 1:10
+count <- 1
+repeat {
+    print(paste(range_vector, collapse = " "))
+    count <- count + 1
+    if (count > 1) {
+        break
+    }
+}
+count <- 1
+while (count == 1) {
+    print(paste(range_vector, collapse = " "))
+    count <- count + 1
+}
+
+# sequences
 print(seq(1, 10, by = 1))
 print(seq(2, 10, length.out = 5))
 
-m <- matrix(1:10, 5, 2, byrow = FALSE, dimnames = list(c('a', 'b', 'c', 'd', 'e'), c('A', 'B')))
+# matrices
+m <- matrix(1:10, 5, 2, byrow = FALSE, dimnames = list(c("a", "b", "c", "d", "e"), c("A", "B")))
 print(dim(m))
 m <- cbind(m, 11:15)
-colnames(m)[3] <- 'C'
+colnames(m)[3] <- "C"
 print(m)
 
-l <- list(c(1:12), c('jan', 'feb', 'mar'), sin)
+# lists
+l <- list(c(1:12), c("jan", "feb", "mar"), sin)
 for (slice in l) {
-    #slices are like [[1]] which is the first row
+    # slices are like [[1]] which is the first row
     print(slice)
 }
 print(length(l))
 
+# arrays
 a <- array(v, dim = c(2, 5))
 print(a)
 
+# data frames
 d <- data.frame(Number = c(1:3), Month = c("Jan", "Feb", "Mar"), Description = c("Cold", "Cold", "Rain"))
 print(d)
 print(str(d))
 print(summary(d))
+
+# input
+goat <- readline("Who's the goat")
+print(paste(goat, "is the goat"))
+
+# scan
+# read.csv()/read.table()/write.csv()/write.table() for csvs
+setwd("C:/users/irons/documents/github/cs-coursework/fhda")
+insertcity <- scan(what="character", file="cis44a/resources/insert_city.sql", nlines=2, nmax=10)
+print(paste(insertcity, collapse = " "))
+
+library("xlsx")
+excel <- read.xlsx(file = "cis44h/sqlspreadsheet.xlsx", sheetIndex = 1)
+print(excel)
