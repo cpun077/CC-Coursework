65a55ffcb79942787669f85f9525e0f4ef05280c
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-01-28 14:45:35 -0800

    check validity of inputs
    check if # of integers to test is a whole number and check if integers to tests are actually integers

--- fhda/cis44h/inputcheck.R
+++ fhda/cis44h/inputcheck.R
@@ -1,11 +1,36 @@
-count <- as.integer(readline("How many integers do you wish to input (whole number please): "))
+# how many integers to ask for
+repeat {
+    data <- readline("How many integers do you wish to input (whole number please): ")
+    # suppressWarnings gets rid of NA coercion warnings
+    num <- suppressWarnings(as.numeric(data))
+    # if num is a valid numeric and is an integer and is a whole number
+    if (!is.na(num) && num %% 1 == 0 && num > 0) {
+        count <- as.integer(data)
+        break
+    } else {
+        print("That is not a whole amount of integers")
+    }
+}
 v <- c()
 
+# which integers are we testing
 while (count > 0) {
-    v <- append(v, as.integer(readline("Input an integer: ")))
+    repeat {
+        data <- readline("Input an integer: ")
+        # suppressWarnings gets rid of NA coercion warnings
+        num <- suppressWarnings(as.numeric(data))
+        # if num is a valid numeric and is an integer
+        if (!is.na(num) && num %% 1 == 0) {
+            v <- append(v, as.integer(data))
+            break
+        } else {
+            print("That is not an integer")
+        }
+    }
     count <- count - 1
 }
 
+# test for bounds and calc square if necessary
 for (i in v) {
     # Assuming range is inclusive
     if (0 <= i && i <= 100) {
