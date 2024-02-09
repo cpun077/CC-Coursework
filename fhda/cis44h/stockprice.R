2f721e0ac3194a8f6171e1c672bf5a4ec8863d79
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-02-09 15:33:25 -0800

    added mean/median and total vol

--- fhda/cis44h/stockprice.R
+++ fhda/cis44h/stockprice.R
@@ -11,16 +11,15 @@ prices <- rev(as.numeric(gsub("\\$", "", data[, 2])))
 days <- rev(as.Date(data[, 1], format = "%m/%d/%Y"))
 volume <- rev(as.numeric(data[, 3]))
 
-# change margins of graph and enable scientific notation
+# change margins of graph
 par(mar = c(10, 10, 10, 10))
-options(scipen = 0)
 
 # plot the line plot of prices vs days
 plot(
     prices, 
     type = "l", 
     xlab = "Days Recorded (2021)", 
-    ylab = "Close/Last Price ($)", 
+    ylab = "Closing Price ($)", 
     xaxt = "n", 
     col = rgb(0, 0, 1)
     )
@@ -37,7 +36,6 @@ axis(
 par(new = TRUE)
 barplot(
     volume,  
-    width = 1, 
     space = 0, 
     col = rgb(1, 0, 0, alpha = 0),
     border = rgb(1, 0, 0),
@@ -46,11 +44,18 @@ barplot(
     )
 
 # add secondary y axis
+ticks <- pretty(volume)
 axis(
     side = 4, 
-    at = pretty(volume),  
-    labels = format(pretty(volume), scientific = TRUE),
+    at = ticks,  
+    labels = ticks,
     cex.axis = 0.7,
     las = 1
     )
-mtext("Volume", side = 4, line = 3)
+mtext("Volume", side = 4, line = 4.5)
+
+# mean/median of closing price
+print(paste("Mean and Median Closing Price are", mean(prices), "and", median(prices), "respectively"))
+
+# total volume of shares traded in 2021
+print(paste("Total Volume of Shares traded in 2021 is", sum(volume)))
