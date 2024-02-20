9bc3386089aa1e45a5474a5f82c88e28c43b4aa4
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-02-19 16:16:42 -0800

    iterative refactoring

--- fhda/cis44h/normhistogram.R
+++ fhda/cis44h/normhistogram.R
@@ -4,32 +4,22 @@ dist1 <- rnorm(100)
 dist2 <- rnorm(100)
 dist3 <- dist1 * dist2
 
+dists <- list(dist1, dist2, dist3)
+colors <- c("red", "blue", "yellow")
+
 # mfrow and mfcol arrange matrices of the plots
 par(mfrow = c(1, 3))
-hist(
-    dist1,
-    main = "Normal Distribution 1",
-    xlab = "Values",
-    col = "red",
-    border = "white"
-)
-hist(
-    dist2,
-    main = "Normal Distribution 2",
-    xlab = "Values",
-    col = "blue",
-    border = "white"
-)
-hist(
-    dist3,
-    main = "Normal Distribution 3",
-    xlab = "Values",
-    col = "yellow",
-    border = "white"
-)
 
-dists <- list(dist1, dist2, dist3)
 for (i in seq_along(dists)) {
+    # plots
+    hist(
+        dists[[i]],
+        main = paste("Normal Distribution", i),
+        xlab = "Values",
+        col = colors[i],
+        border = "white",
+    )
+    # statistics
     print(paste("Normal Distribution", i))
     print(paste("Mean:", mean(dists[[i]])))
     print(paste("Median:", median(dists[[i]])))
