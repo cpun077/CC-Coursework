716a9a9485446d4dce9c4a7831f428d6ac7b4c6a
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-02-26 18:36:08 -0800

    fix thermometer bug

--- fhda/cis44h/thermometer.R
+++ fhda/cis44h/thermometer.R
@@ -1,18 +1,19 @@
 # Caden Pun 2/24/24
 
 cToF <- function(c) {
-    return(c * 9 / 5 +32)
+    return(c * 9 / 5 + 32)
 }
 
 formatData <- function(min, max, interval) {
     c <- min
     # data frame allows built-in column headers
     result <- data.frame(Celcius = numeric(), Farenheit = numeric())
-    while (c <= max) {
-        c <- c + interval
+    while (floor(c) <= max) {
         result <- rbind(result, data.frame(Celcius = c, Farenheit = cToF(c)))
+        c <- c + interval
     }
     return(round(result, digits = 2))
 }
 
+print(formatData(0, 100, 20))
 print(formatData(0, 100, 100/7))
