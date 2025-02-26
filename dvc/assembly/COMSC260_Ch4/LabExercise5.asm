e49d72acc794a9bba3d48a470475bf79e0f4e1ad
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2025-02-25 17:32:17 -0800

    lab exercise 5
    movzx, movsx, xchg

---
+++ dvc/assembly/COMSC260_Ch4/LabExercise5.asm
@@ -0,0 +1,32 @@
+; Assignment #: Lab Exercise 5
+; Program Description: 
+; Author: Caden Pun (student: 2164581)
+; Creation Date: 2/25/25
+; Revisions: 0
+; Date:              Modified by:
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+.data
+intA word 100
+intB sword -100
+arrayD dword 1,2,3,4
+.code
+main PROC
+	movzx ebx, intA
+	movsx edx, intB
+
+	mov eax, arrayD
+	xchg eax, arrayD+12
+	mov arrayD, eax
+
+	mov eax, arrayD+4
+	xchg eax, arrayD+8
+	mov arrayD+4, eax
+
+	INVOKE ExitProcess,0
+main ENDP
+; (insert additional procedures here)
+END main
